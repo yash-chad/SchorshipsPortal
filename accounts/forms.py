1c4eb65b13f162beca23987b7c8b39b1bc6c9df1
SiddhiJhunjhunwala <siddhi2000jhun@gmail.com>
2020-04-26 12:52:17 +0530

    Add fields

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,7 +5,9 @@ from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    first_name = forms.CharField(required = True)
+    last_name = forms.CharField(required = True)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2', )
+        fields = ('first_name','last_name' , 'username', 'email', 'password1', 'password2')
