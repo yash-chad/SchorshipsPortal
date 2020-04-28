aa0f865750c22d0d0f3483ba5f7fecd6f186aa13
SiddhiJhunjhunwala <siddhi2000jhun@gmail.com>
2020-04-28 11:17:36 +0530

    working

--- accounts/views.py
+++ accounts/views.py
@@ -56,13 +56,13 @@ def signup(request):
             })
 
             to_email = form.cleaned_data.get('email')
-            me = 'variegatedlark@gmail.com'
-            my_password = "rockhopp"
+            me = ''
+            my_password = ""
             you = to_email
 
             msg = MIMEMultipart('alternative')
             msg['Subject'] = "Activate your blog account."
-            msg['From'] = 'variegatedlark@gmail.comm' 
+            msg['From'] = '' 
             msg['To'] = to_email
             part2 = MIMEText(message, 'html')
             msg.attach(part2)
